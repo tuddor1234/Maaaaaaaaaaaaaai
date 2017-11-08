3d680434ff42798db0e1f4151c118524a78ba260
tuddor1234 <tuddor1234@yahoo.com>
2017-11-08 22:31:01 +0200

    Initial commit

---
+++ Assets/Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+
+    public AudioClip mai;
+    public AudioClip speriat;
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnButtonMaiPressed()
+    {
+        audioSource.clip = mai;
+        audioSource.Play();
+    }
+
+    public void OnButtonTeaiSperiatPressed()
+    {
+        audioSource.clip = speriat;
+        audioSource.Play();
+    }
+}
